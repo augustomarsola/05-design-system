5a901d565fb088f143933220858987756f5cdecd
Augusto Marsola <augusto.marsola@live.com>
2022-10-19 21:25:02 -0300

    Added full tokens

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,1 +1,9 @@
-export * from './colors';
+export * from './colors'
+
+export * from './spaces'
+export * from './radios'
+
+export * from './fonts'
+export * from './font-sizes'
+export * from './font-weights'
+export * from './line-heights'
